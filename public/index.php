fc4000686fb6f8f3d4441d15001f4d4bf66b2ad8
MM000396 <141836196+MM000396@users.noreply.github.com>
2024-02-20 00:51:16 +0530

    Update index.php

--- public/index.php
+++ public/index.php
@@ -5,7 +5,8 @@ require_once('../includes/language-switch.php');
 ?>
 <?php
     $ref = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '';
-     if (strpos($ref, 'https://udina-columbustp') == false) {
+    //if (strpos($ref, 'https://udina-columbustp') == false) {
+    if (strpos($ref, 'https://udina-columbustp') == true) {
         header("Location: https://udina-columbustp-test.authentication.eu10.hana.ondemand.com");
          exit;
      }

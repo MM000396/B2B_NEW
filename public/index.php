05b04545a7bbb8d0f40bff04cc70a10dbf68333c
MM000396 <141836196+MM000396@users.noreply.github.com>
2024-02-20 01:18:31 +0530

    Update index.php

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@ require_once('../includes/language-switch.php');
 <?php
     $ref = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '';
     if ( (strpos($ref, 'https://udina-columbustp') == false) && (strpos($ref, 'https://b2b-login-dev.columbustp.com') == false) ) {
-        header("Location: https://udina-columbustp-test.authentication.eu10.hana.ondemand.com");
+        //header("Location: https://udina-columbustp-test.authentication.eu10.hana.ondemand.com");
          exit;
      }
 ?>
